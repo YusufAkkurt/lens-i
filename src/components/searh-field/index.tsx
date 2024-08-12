ca4ea8771c36aa2a2986cabfb25e835a3c87e0d5
Yusuf Akkurt <ysfakkurt@hotmail.com>
2024-08-12 13:17:33 +0300

    search inptu tamamlandı

--- src/components/searh-field/index.tsx
+++ src/components/searh-field/index.tsx
@@ -10,10 +10,17 @@ export const SearchField = () => {
 
 	return (
 		<form
+			method="GET"
+			action="/search"
 			onSubmit={(event) => {
 				event.preventDefault();
+
+				const q = (event.currentTarget.q as HTMLInputElement).value.trim();
+				if (!q) return;
+
+				router.push(`/search?q=${encodeURIComponent(q)}`);
 			}}
-			className="max-w-[30rem] w-full">
+			className="max-w-[25rem] w-full">
 			<div className="relative">
 				<Input name="q" placeholder="Search" className="ps-10 bg-primary-foreground" />
 				<SearchIcon className="size-5 absolute start-3 top-1/2 -translate-y-1/2 transform text-muted-foreground" />
